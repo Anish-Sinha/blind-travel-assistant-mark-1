38823247b29d77671b40852790772974df8a858b
Anish-Sinha <anish.sinha714@gmail.com>
2019-09-17 19:03:49 +0000

    Update ocr.py

--- ocr.py
+++ ocr.py
@@ -20,13 +20,3 @@ translator = Translator()
 translation = translator.translate(word, dest='fr') #to change the language, insert langauge code inside the '__'
 print "\nText translated to French"
 print translation.text
-
-#This places a bounding box on the image that OCR was performed on
-d = pyt.image_to_data(img, output_type=Output.DICT)
-n_boxes = len(d)
-for i in range(n_boxes):
-	(x, y, w, h) = (d['left'][i], d['top'][i], d['width'][i], d['height'][i] )
-	cv2.rectangle(img, (x,y), (x+w, y+h), (0,255,0), 2)
-
-cv2.imshow('img', img)
-cv2.waitKey(0)
